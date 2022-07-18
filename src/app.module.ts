847d1b3a5f42cc961cc93f468ccd029a040ee600
Yusuf Ganiyu <airscholar@gmail.com>
2022-07-18 17:35:30 +0100

    feat: added todosModule to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
+import { DbModule } from './db/db.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
+  imports: [DbModule, TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
